a4c918c917bbe4269ca6ab1fb25f85b1540cb973
Aneeq Ur Rehman <raoaneeq573@gmail.com>
2026-07-18 03:02:02 +0500

    Adapt entry points for vercel deployment

--- index.jsx
+++ index.jsx
@@ -6,4 +6,12 @@ app.get('/', (req, res) => { res.json({message: 'Health check API is running. se
 
 app.get('/health', (req, res) => { res.json({Status: 'OK', timestamp: new Date().toISOString(),});});
 
-app.listen(PORT, () => { console.log(`Server is running on http://localhost:${PORT}`); });
+//app.listen(PORT, () => { console.log(`Server is running on http://localhost:${PORT}`); });
+
+if (require.main == module) {
+    app.listen(PORT, () => {
+        console.log(`Server is running on http://localhost:${PORT}`);
+    });
+}
+
+module.exports = app;
