ce60ffdb69a5df03b5193e4656095f366cbccd7a
Aneeq Ur Rehman <raoaneeq573@gmail.com>
2026-07-19 15:51:44 +0500

    Updates to check working through vercel

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => { res.json({message: 'Health check API is running. see /health'});});
+
+app.get('/health', (req, res) => { res.json({Status: 'OK', timestamp: new Date().toISOString(),});});
+
+//app.listen(PORT, () => { console.log(`Server is running on http://localhost:${PORT}`); });
+
+if (require.main === module) {
+    app.listen(PORT, () => {
+        console.log(`Server is running on http://localhost:${PORT}`);
+    });
+}
+
+module.exports = app;
